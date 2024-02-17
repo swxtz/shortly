cb2240ebc3e739beb047ea778dbdbd549b16046d
Gustavo Mendonça <dev.gustavomendonca@protonmail.com>
2024-02-17 19:39:54 -0300

    feat: create route to fetch url

--- backend/src/routes/links/index.ts
+++ backend/src/routes/links/index.ts
@@ -1,24 +1,63 @@
-import type { FastifyInstance } from "fastify";
+import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
+import { randomString } from "../../utils/random-strings";
 
 export async function linksRoutes(app: FastifyInstance) {
-    app.post("/create/free", async (request, reply) => {
-        const schema = z.object({
-            url: z.string(),
-        });
+    app.post(
+        "/create/free",
+        async (request: FastifyRequest, reply: FastifyReply) => {
+            const schema = z.object({
+                url: z.string(),
+            });
 
-        try {
-            const { url } = schema.parse(request.body);
-            const id = Math.random().toString(36).substr(2, 5);
-            await app.redis.set(id, url);
-            reply.send({ id });
-        } catch (error) {
-            if (error instanceof z.ZodError) {
-                reply.status(400).send({ message: error });
-                return;
+            try {
+                const { url } = schema.parse(request.body);
+                let shortLinkCode = randomString(10);
+
+                const verifyKey = await app.redis.get(shortLinkCode);
+
+                if (verifyKey) {
+                    shortLinkCode = randomString(10);
+                }
+
+                app.redis.setex(shortLinkCode, 60 * 30, url);
+                reply.send({ shortLink: shortLinkCode });
+            } catch (error) {
+                if (error instanceof z.ZodError) {
+                    reply.status(400).send({ message: error });
+                    return;
+                }
+                console.error(error);
+                reply.status(400).send({ message: "Invalid request" });
             }
-            console.error(error);
-            reply.status(400).send({ message: "Invalid request" });
-        }
-    });
+        },
+    );
+
+    app.get(
+        "/free/:key",
+        async (request: FastifyRequest, reply: FastifyReply) => {
+            const schema = z.object({
+                key: z.string(),
+            });
+
+            try {
+                const { key } = schema.parse(request.params);
+                const url = await app.redis.get(key);
+
+                if (!url) {
+                    reply.status(404).send({ message: "Link not found" });
+                    return;
+                }
+
+                reply.send({ url });
+            } catch (error) {
+                if (error instanceof z.ZodError) {
+                    reply.status(400).send({ message: error });
+                    return;
+                }
+                console.error(error);
+                reply.status(400).send({ message: "Invalid request" });
+            }
+        },
+    );
 }
---
+++ backend/src/utils/random-strings.ts
@@ -0,0 +1,11 @@
+export function randomString(length: number) {
+    const characters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * characters.length),
+        );
+    }
+    return result;
+}
